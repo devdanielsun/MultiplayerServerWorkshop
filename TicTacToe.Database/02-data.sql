89c468426d9acc05a6cd6ea1c18cf0d1bf363e3a
devdanielsun <g.daniel.geerts@gmail.com>
2025-05-23 09:29:40 +0200

    Update database default values

--- TicTacToe.Database/02-data.sql
+++ TicTacToe.Database/02-data.sql
@@ -6,10 +6,10 @@ VALUES ('player1'),
 -- Insert a finished game
 -- The board represents a completed game where 'X' has won (e.g., top row filled with 'X')
 INSERT INTO games (game_name, board, is_active, winner_id) 
-VALUES ('Default Match 1', '11122____', FALSE, 1)
-        ('Default Match 2', '_________', TRUE, 1);
+VALUES ('Default Match 1', '11122____', FALSE, 1),
+        ('Default Match 2', '22211____', FALSE, 2);
 
 -- Insert a player pair for the game
 INSERT INTO games_players (game_id, player_one_id, player_two_id, player_turn) 
 VALUES (1, 1, 2, 1),
-        (2, 1, NULL, 1);
+        (2, 2, 1, 1);
